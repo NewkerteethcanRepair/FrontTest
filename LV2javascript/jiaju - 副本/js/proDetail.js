675f483deb2add29704f48a8a50e58500458209d
WIN-GIFGCOL062U\Administrator <862774887@qq.com>
2020-04-20 19:53:51 +0800

    1.9

---
+++ LV2javascript/jiaju - 副本/js/proDetail.js
@@ -0,0 +1,48 @@
+$(function () {
+    
+    getFl()
+    getbottomimg();
+    taball();
+})
+
+function getFl() {
+    $(".smallImg").on("click", ".fl", function () {
+        // console.log("sd");
+        
+        $(this).toggleClass("fl-clicked")
+        var imgsrc=$(this).find("img").attr("src");
+        $(".det").attr("src",imgsrc);
+
+        $(this).siblings().removeClass("fl-clicked")
+    })
+}
+
+function getbottomimg(){
+    $(".proDet").find(".smallImg img").hover(
+        function() {
+            $(this).addClass("fl-clicked")
+            var imgsrc=$(this).attr("src");
+            $(".det").attr("src",imgsrc);
+        },
+        function() {
+            $(this).removeClass("fl-clicked")
+        }
+    )
+}
+
+function taball(){
+    $(".msgTit").on("click","a",function(){
+        $(this).addClass("on");
+        $(this).siblings().removeClass("on");
+        if($(this).text()=="所有评价"){
+            console.log(123);
+            $(".eva").css("display","block");
+            $(".msgImgs").css("display","none")
+        }
+        else{
+            $(".eva").css("display","none");
+            $(".msgImgs").css("display","block")
+        }
+    })
+}
+

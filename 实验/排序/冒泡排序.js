4039097400b10910e79d2a60f6081cca9cbb3d9f
NewkerteethcanRepair <37927353+NewkerteethcanRepair@users.noreply.github.com>
2020-05-08 19:00:59 +0800

    5.819

---
+++ 实验/排序/冒泡排序.js
@@ -0,0 +1,16 @@
+
+var num=[1,2,5,7,23,123,12,33,22,11,99];
+
+for(var i=0;i<num.length-1;i++){
+    // 第一次遍历后 最大的数会放在最后
+    for(var j=0;j<num.length-1-i;j++){
+        if(num[j]>num[j+1]){
+            var d=num[j];
+            num[j]=num[j+1];
+            num[j+1]=d;
+        }
+
+    }
+}
+
+console.log(num);
---
+++ 实验/排序/选择排序.js
@@ -0,0 +1,20 @@
+// 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+// 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+// 重复第二步，直到所有元素均排序完毕。
+
+var num=[1,2,5,7,23,123,12,33,22,11,99];
+var len = num.length;
+var minIndex, temp;
+for (var i = 0; i < len - 1; i++) {
+    minIndex = i;
+    for (var j = i + 1; j < len; j++) {
+        if (num[j] < num[minIndex]) {     // 寻找最小的数
+            minIndex = j;                 // 将最小数的索引保存
+        }
+    }
+    temp = num[i];
+    num[i] = num[minIndex];
+    num[minIndex] = temp;
+}
+
+console.log(num);
